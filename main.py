05d8e16e6bf6f887efec0eb96e13dd0a10d5e773
TuTurDuDur <arthur.p.hagnery@gmail.com>
2023-09-10 19:25:23 +0200

    D'autres trucs

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
 import os
 from utils import *
 
-class FileExplorer():
-    def getFunctionContent(self,path: str) -> str:
-        with open(path + (".mcfuntion" if not path.endswith(".mcfunction") else ""), "r") as file:
+class FileExplorer:
+    def getFunctionContent(self, path: str) -> str:
+        with open(path + (".mcfunction" if not path.endswith(".mcfunction") else ""), "r") as file:
             return file.read()
 
     def createFunction(self, path: str, content: str) -> None:
-        file = open(path + ".mcfunction","w")
+        file = open(path + ".mcfunction", "w")
         file.write(content)
         file.close()
 
-
-    def createSubFunction(self, root_path : str, content: str, index: str) -> None:
-        self.createFunction(root_path + "/" + indexToUUID(index), content)
+    def createSubFunction(self, rootPath: str, content: str, index: str) -> None:
+        self.createFunction(rootPath + "/" + indexToUUID(index), content)
 
 explorer = FileExplorer()
 
 # Turns custom function to actual code
-def format(src_path,namespace):
-    target_path = "./result/"+src_path
-    os.makedirs(os.path.dirname(target_path), exist_ok=True)
-    
-    content = explorer.getFunctionContent(src_path)
+def format(srcPath, namespace):
+    targetPath = "./result/" + srcPath
+    os.makedirs(os.path.dirname(targetPath), exist_ok=True)
 
-    current_chunk = ""
+    content = explorer.getFunctionContent(srcPath)
+
+    currentChunk = ""
 
     for line in content:
         if line.startswith("label"):
-            current_chunk.append(f"function {namespace}:{}")
-
-
-    
-
-
-
-
-
-
-format("function")
+            currentChunk.append(f"function {namespace}:{}")
