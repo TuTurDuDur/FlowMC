a4677b2795307ca0726c7a719c739dd2e148ac2b
TuTurDuDur <arthur.p.hagnery@gmail.com>
2023-09-13 17:36:54 +0200

    Trucs

--- main.py
+++ main.py
@@ -6,46 +6,68 @@ class FileExplorer: # Automatically adds file extension to the paths
         with open(path + (".flowmc" if not path.endswith(".mcfunction") else ""), "r") as file:
             return file.read()
 
-    def createFunction(self, path: str, content: str) -> None:
-
+    def createFunctionFile(self, path: str, content: str) -> None:
         file = open(path + ".mcfunction", "w")
         file.write(content)
         file.close()
 
-    def createSubFunction(self, rootPath: str, functionName: str, content: str, index: int) -> None:
+    def createSubFunctionFile(self, rootPath: str, functionName: str, content: str, index: int) -> None:
         os.makedirs(rootPath + "/" + functionName, exist_ok=True)
         self.createFunction(rootPath + "/" + functionName + "/" + indexToUUID(index), content)
 
-explorer = FileExplorer()
+EXPLORER = FileExplorer()
+
+class SubFunction:
+    def __init__(self, path, namespace, UUID):
+        self.path = path
+        self.UUID = UUID
+        self.namespace = namespace
+        self.content = ""
+    
+    def addLine(self,line):
+        if line != "": self.content += "\n"+line
+    
+    def addSubFunctionReference(self,subFunctionUUID):
+        self.addLine(f'function { self.namespace }:{ self.path }/{subFunctionUUID}')
+
+    def createFile(self):
+        EXPLORER.createSubFunctionFile(self.path, self.subFunctionName)
+    
+    def reset(self):
+        content = ""
+
+
+    
 
 # Turns custom function to actual code
 
 def format(srcPath, targetPath, namespace="test"):
     # Set up
     os.makedirs(os.path.dirname(targetPath), exist_ok=True) # creates the path to the final mcfunction files
-    content = explorer.getFunctionContent(srcPath).split("\n")
+    content = EXPLORER.getFunctionContent(srcPath).split("\n")
 
     # Loop variables
-    currentChunk = "" # Body of the current subfunction
-    knownLabels = {} # {labelName : corresponding subfun path}. ig : {"firstlabel" : "" , "secondlabel": "/0"...}
+    knownLabels = {} # {labelName : corresponding subfunc path}. ig : {"firstlabel" : "" , "secondlabel": "/0"...}
     FUNCTION_NAME = srcPath.split("/")[-1]
 
 
-    explorer.createFunction(f'{ targetPath }/{ FUNCTION_NAME }',
+    EXPLORER.createFunctionFile(f'{ targetPath }/{ FUNCTION_NAME }',
     f'function { namespace }:{ FUNCTION_NAME }/{indexToUUID(0)}')
+    
+    currentFunction = SubFunction(targetPath, namespace, indexToUUID(0))
 
     for lineNumber, line in enumerate(content):
         if line.startswith("label"):
             labelName = line [6:]
-            knownLabels.update({labelName : indexToUUID(len(knownLabels))})
-            labelsMet += 1 
-            
-               
+            knownLabels.update({labelName : (UUID := indexToUUID(len(knownLabels)))})
 
-            continue
+            if currentFunction.content != "":
+                currentFunction.addSubFunctionReference(UUID)          
+            currentFunction.reset()
+
 
         elif line.startswith("goto"):
-            pass
+            targetLabelName = line[5:]
 
         else:
             if line != "": currentChunk += "\n"+line
