dda9e409bc0ab9fdbf3330b244050b8537760d17
TuTurDuDur <arthur.p.hagnery@gmail.com>
2023-09-16 18:17:43 +0200

    CA MARCHE !!!!! :D

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ import os
 from utils import *
 
 class FileExplorer: # Automatically adds file extension to the paths
-    def getFunctionContent(self, path: str) -> str:
+    def getScriptContent(self, path: str) -> str:
         with open(path + (".flowmc" if not path.endswith(".mcfunction") else ""), "r") as file:
             return file.read()
 
     def createFunctionFile(self, path: str, content: str) -> None:
+        try :
+            file = open(path + ".mcfunction", "r")
+            if (content:=file.read())=="": raise WtfError(f"File is already filled up : {content}")
+            file.close()
+        except:
+            pass
+
         file = open(path + ".mcfunction", "w")
         file.write(content)
         file.close()
 
-    def createSubFunctionFile(self, path: str, content: str, subFunctionID: str) -> None:
-        os.makedirs(path, exist_ok=True)
-        self.createFunction(path+ "/" + subFunctionID, content)
+    def createSubFunctionFile(self, motherFunctionPath: str, content: str, subFunctionID: str) -> None:
+        os.makedirs(motherFunctionPath, exist_ok=True)
+        self.createFunctionFile(motherFunctionPath+ "/" + subFunctionID, content)
 
 EXPLORER = FileExplorer()
 
 class SubFunction:
-    def __init__(self, path, namespace, UUID):
+    def __init__(self, path, namespace, /, UUID, motherName,):
         self.path = path
         self.UUID = UUID
         self.namespace = namespace
         self.content = ""
+        self.motherName = motherName
     
     def addLine(self,line):
         if line != "": self.content += "\n"+line
@@ -31,7 +39,7 @@ class SubFunction:
         self.addLine(f'function { self.namespace }:{ self.path }/{subFunctionUUID}')
 
     def createFile(self):
-        EXPLORER.createSubFunctionFile(self.path, self.content, self.UUID)
+        EXPLORER.createSubFunctionFile(self.path+"/"+self.motherName, self.content, self.UUID)
         self._reset()
     
     def _reset(self):
@@ -42,45 +50,50 @@ class SubFunction:
 
 # Turns custom function to actual code
 
-def format(srcPath, targetPath, namespace="test"):
+def format(srcPath, targetPath,namespace="test"):
+    unsureGoto = []
     # Set up
     os.makedirs(os.path.dirname(targetPath), exist_ok=True) # creates the path to the final mcfunction files
-    content = EXPLORER.getFunctionContent(srcPath).split("\n")
+    content = EXPLORER.getScriptContent(srcPath).split("\n")
 
     # Loop variables
-    knownLabels = {} # {labelName : corresponding subfunc path}. ig : {"firstlabel" : "" , "secondlabel": "/0"...}
-    FUNCTION_NAME = srcPath.split("/")[-1]
+    knownLabels = {"#_start":0} # {labelName : corresponding subfunc path}. ig : {"firstlabel" : "" , "secondlabel": "/0"...}
 
-
+    FUNCTION_NAME = srcPath.split("/")[-1]    
     EXPLORER.createFunctionFile(f'{ targetPath }/{ FUNCTION_NAME }',
-    f'function { namespace }:{ FUNCTION_NAME }/{indexToUUID(0)}')
-    
-    currentFunction = SubFunction(targetPath, namespace, indexToUUID(0))
+    f'function { namespace }:{ FUNCTION_NAME }/0')
+
+
+    currentFunction = SubFunction(targetPath, namespace, UUID = indexToUUID(0), motherName = FUNCTION_NAME)
 
     for lineNumber, line in enumerate(content):
         if line.startswith("label"):
             labelName = line [6:]
-
-            UUID = indexToUUID(len(knownLabels))
-            knownLabels.update({labelName : UUID})
+            if labelName not in knownLabels:
+                UUID = indexToUUID(len(knownLabels))
+                knownLabels.update({labelName : UUID})
+            else:
+                UUID = knownLabels[labelName]
             
             if currentFunction.content != "":
                 currentFunction.addSubFunctionReference(UUID)
                 currentFunction.createFile()
+            currentFunction.UUID = UUID
+
 
 
         elif line.startswith("goto"):
             targetLabelName = line[5:]
-            if targetLabelName not in knownLabels.keys():
+            if targetLabelName not in knownLabels:
                 UUID = indexToUUID(len(knownLabels))
-                knownLabels.update({labelName: UUID})
+                knownLabels.update({targetLabelName: UUID})
             else:
-                UUUID = knownLabels[targetLabelName]
+                UUID = knownLabels[targetLabelName]
+                unsureGoto.append(UUID)
             currentFunction.addSubFunctionReference(UUID)
             currentFunction.createFile()
-
 
         else:
             currentFunction.addLine(line)
 
-format("./examples/myfunction","./examples/result")
+format("examples/myfunction","examples/result")
